1dcffcb95b33b1502278ba909314c08c9266a753
Robert Lee Russell jr <144120738+PlatinumWrist5055@users.noreply.github.com>
2023-10-13 16:26:10 -0500

    Initialize dependabot/npm_and_yarn/node-sass-7.0.0

---
+++ brave/ui/app/store/actions.js
@@ -0,0 +1,56 @@
+const MetaMaskActions = require('../../../../ui/app/store/actions')
+
+MetaMaskActions.addToken = addToken
+MetaMaskActions.setBatTokenAdded = setBatTokenAdded
+MetaMaskActions.SET_BAT_TOKEN_ADDED = 'SET_BAT_TOKEN_ADDED'
+
+MetaMaskActions.showModal = showModal
+
+function setBatTokenAdded () {
+  return (dispatch) => {
+    background.setBatTokenAdded((err) => {
+      if (err) {
+        return dispatch(actions.displayWarning(err.message))
+      }
+    })
+    dispatch({
+      type: actions.SET_BAT_TOKEN_ADDED,
+      value: true
+    })
+  }
+}
+
+function addToken (address, symbol, decimals, image) {
+  return (dispatch) => {
+    dispatch(actions.showLoadingIndication())
+    return new Promise((resolve, reject) => {
+      background.addToken(address, symbol, decimals, image, (err, tokens) => {
+        dispatch(actions.hideLoadingIndication())
+        if (err) {
+          dispatch(actions.displayWarning(err.message))
+          reject(err)
+        } else if (symbol === 'BAT') {
+          dispatch(actions.setBatTokenAdded())
+        }
+        dispatch(actions.updateTokens(tokens))
+        resolve(tokens)
+      })
+    })
+  }
+}
+
+function showModal (payload) {
+  if (payload.name === 'METAMETRICS_OPT_IN_MODAL') {
+    return {
+      type: '',
+      payload: {}
+    }
+  }
+
+  return {
+    type: MetaMaskActions.MODAL_OPEN,
+    payload,
+  }
+}
+
+module.exports = MetaMaskActions
